5f34a1d13a35f2606dad36a83436982b615a1b11
HeinrichDev6002 <tutorgames.gmil.com@gmail.com>
2025-06-26 17:51:22 -0300

    First version, by: me

---
+++ src/WordsGenerator.java
@@ -0,0 +1,30 @@
+/**
+ * @author: Henrique S.M
+ * This project is made for Izanagi
+ */
+interface Alphabet{
+    final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+}
+
+public class WordsGenerator implements Alphabet {
+
+    /*
+     *
+     */
+    public static String generate(int[] i) throws Exception {
+        StringBuilder str = new StringBuilder();
+        if (i.length != 0) {
+            for (int j = 0; j < i.length; j++) {
+                if (i[j] > ALPHABET.length()) {
+                    throw new Exception("Posição inválida");
+                }
+                if (i[j] == ALPHABET.length()) {
+                    str.append(" ");
+                }
+                str.append(ALPHABET.charAt(i[j]));
+            }
+            return str.toString();
+        }
+        return null;
+    }
+}
